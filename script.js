f0a79d39d1a706bdcfc54b21debff11d5b31f783
charon <cariasjosue@gmail.com>
2022-01-15 18:07:08 -0500

    redo

--- script.js
+++ script.js
@@ -1,66 +1,96 @@
 // Assignment code here
 
+var randompass = { 
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  
+};
+
+var randomLower =
+   String.fromCharCode(Math.floor(Math.random() *26)+ 97 );
+
+
+var randomUpper =
+  String.fromCharCode(Math.floor(Math.random() *26)+ 65 );
+
+var randomNumber =
+  String.fromCharCode(Math.floor(Math.random() *10)+ 48 );
+
+
+
+
+
+
+
+
+
 function generatePassword() {
+  promptCharacter();
+};
+
+
+
 // Prompts user to choose character length
-var promptCharacter = window.prompt(" Please choose between 8 - 128 characters.");
+function promptCharacter() {
+var promptChar = window.prompt(" Please choose between 8 - 128 characters.");
 // Converts the value into number
-promptCharacter = parseInt(promptCharacter);
-console.log (promptCharacter);
-// validate prompt answer
-/* if (promptCharacter === "" || promptCharacter === null) {
-  window.alert("You need to provide a valid answer! Please try again.");
-  // use return to call it again and stop the rest of this function from running
-  return generatePassword();
-} */
+promptChar = parseInt(promptChar);
 // if proper values imputted then proceeds towards next parameters
-if (promptCharacter <= 128 && promptCharacter >= 8) {
-  characterLower(); 
+if (promptChar <= 128 && promptChar >= 8) {
+  characterLower(promptChar); 
 }
   // Checks to see if a numeric value was entered
   else (isNaN(promptCharacter)); {
     window.alert("Please enter a numeric value between 8-128.");
-    // use return to call it again and stop the rest of this function from running
     return generatePassword();
   }
+  
 };
 
-function characterLower() {
+// Function to choose owercase
+function characterLower(randomLower) {
+  
   var charLow = window.confirm("Would you like LowerCase letters?");
-  if (charLow) {
-    characterUpper();
+  if (charLow) {  
+    characterUpper(randomLower);   
   } else {
     window.alert("No lowercase selected");
     characterUpper();
-  }
+  }  
 };
 
-function characterUpper() {
+// Function to choose ppercase
+function characterUpper(randomUpper) {
   var charUpp = window.confirm("Would you like UpperCase letters?");
   if (charUpp) {
-    characterNumber();
+    characterNumber(randomUpper);
   } else {
     window.alert("No Uppercase selected");
     characterNumber();
   }
+  console.log(characterNumber);
 };
 
-function characterNumber() {
+// Function to choose numbers
+function characterNumber(randomNumber) {
   var charNum = window.confirm("Would you like Numbers?");
   if (charNum) {
-    characterSpecial();
+    characterSpecial(randomNumber);
   } else {
     window.alert("No Numbers selected");
     characterSpecial();
   }
 };
 
-function characterSpecial() {
+// Function to choose special characters
+function characterSpecial(randomsymbol) {
   var charSpec = window.confirm("Would you like Special Characters?");
-  if (charLow) {
-    characterHigh();
+  if (charSpec) {
+    generatedPassword(randomsymbol);
   } else {
     window.alert("No Special Charcters selected");
-    characterHigh();
+    generatedPassword();
   }
 };
 
